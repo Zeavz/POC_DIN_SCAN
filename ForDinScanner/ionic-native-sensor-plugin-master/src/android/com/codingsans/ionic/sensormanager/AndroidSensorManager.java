dc2e31031dd2b6e3abc1da2e2c1a69997a82c931
Jenis Jesuratnam <Jenis_Jesuratnam@hotmail.com>
2018-08-06 22:58:17 -0400

    DinScannerPlugin

---
+++ ForDinScanner/ionic-native-sensor-plugin-master/src/android/com/codingsans/ionic/sensormanager/AndroidSensorManager.java
@@ -0,0 +1,142 @@
+package com.codingsans.ionic.sensormanager;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import android.Manifest;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+
+import android.content.pm.PackageManager;
+
+public class AndroidSensorManager extends CordovaPlugin {
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private CallbackContext callbackContext;
+    private JSONObject data = new JSONObject();
+    private CameraScreenFrag frag;
+    private int containerViewId = 20;
+    private String drugName = "";
+
+    private static final String [] permissions = {
+        Manifest.permission.CAMERA
+      };
+
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        mSensorManager = (SensorManager) cordova.getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for(int r:grantResults){
+            if(r == PackageManager.PERMISSION_DENIED){
+                cordova.getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                    final android.widget.Toast toast = android.widget.Toast.makeText(
+                      cordova.getActivity().getWindow().getContext(),
+                      "Something went wrong getting permissions",
+                      android.widget.Toast.LENGTH_LONG 
+                        );
+                        toast.show();
+                    }
+                });
+              return;
+            }
+          }
+          if (requestCode == 0) {
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                final android.widget.Toast toast = android.widget.Toast.makeText(
+                  cordova.getActivity().getWindow().getContext(),
+                  "Permission check 2 success",
+                  android.widget.Toast.LENGTH_LONG 
+                    );
+                    toast.show();
+                }
+            });
+          }
+    }
+
+    @Override
+    public void onDestroy() {
+        mSensorManager.unregisterListener(listener);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("start".equals(action)) {
+            mSensorManager.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        } else if ("stop".equals(action)) {
+            if (cordova.hasPermission(permissions[0])) {
+                return startScanner();
+              } else {
+                cordova.requestPermissions(this, 0, permissions);
+                return true;
+              }
+              //startScanner();
+        } else if ("getCurrent".equals(action)) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, this.data);
+            callbackContext.sendPluginResult(result);
+            return true;
+        }
+        return false;  // Returning false results in a "MethodNotFound" error.
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+          if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+              data = new JSONObject();
+              try {
+                  data.put("x", event.values[0]);
+                  data.put("y", event.values[1]);
+                  data.put("z", event.values[2]);
+              } catch(JSONException e) {}
+          }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // unused
+        }
+    };
+
+    private boolean startScanner(){
+        frag = new CameraScreenFrag();
+
+        FrameLayout containerView = (FrameLayout)cordova.getActivity().findViewById(containerViewId);
+        if(containerView == null){
+            containerView = new FrameLayout(cordova.getActivity().getApplicationContext());
+            containerView.setId(containerViewId);
+            FrameLayout.LayoutParams containerLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            cordova.getActivity().addContentView(containerView, containerLayoutParams);
+        }
+
+        FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(containerView.getId(), frag);
+        fragmentTransaction.commit();
+        return true;
+    }
+
+    public void setDrugName(String name){
+        this.drugName = name;
+    }
+}
---
+++ ForDinScanner/ionic-native-sensor-plugin-master/src/android/com/codingsans/ionic/sensormanager/CameraScreenFrag.java
@@ -0,0 +1,224 @@
+package com.codingsans.ionic.sensormanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import android.content.Context;
+
+//Vision Stuff
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//Vision Stuff
+
+//Camera Stuff
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.SurfaceHolder;
+import android.util.SparseArray;
+//Camera Stuff
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CameraScreenFrag extends Fragment{
+   
+    View viewer;
+    String appResourcesPackage;
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+    boolean hide = true;
+    String url = "https://health-products.canada.ca/api/drug/drugproduct/?lang=en&type=json&din=";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        appResourcesPackage = getActivity().getPackageName();  
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+            final android.widget.Toast toast = android.widget.Toast.makeText(
+              getActivity().getWindow().getContext(),
+              "I am inside the camera fragment",
+              android.widget.Toast.LENGTH_LONG 
+                );
+                toast.show();
+            }
+        });
+        viewer = inflater.inflate(getResources().getIdentifier("camera_view_screen", "layout", appResourcesPackage), container, false);
+        setUpCameraScreen();
+        return viewer;
+    }
+
+    protected void apiCall(String id){
+
+        String tempUrl = url + id.substring(4);
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getWindow().getContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null) {
+                            try{
+                                JSONArray objarr = new JSONArray(response.toString());
+                                JSONObject obj = objarr.getJSONObject(0);
+                                String toOutput = obj.getString("brand_name");
+                                // returnIntent.putExtra("result", toOutput);
+                                // returnIntent.putExtra("result2", obj.getString("drug_identification_number"));
+                                // returnIntent.putExtra("result3", obj.getString("company_name"));
+                                // returnIntent.putExtra("result4", obj.getString("descriptor"));
+                                getActivity().runOnUiThread(new Runnable() {
+                                    public void run() {
+                                    final android.widget.Toast toast = android.widget.Toast.makeText(
+                                      getActivity().getWindow().getContext(),
+                                      "Drug found: " + toOutput,
+                                      android.widget.Toast.LENGTH_LONG 
+                                        );
+                                        toast.show();
+                                    }
+                                });
+                            }
+                            catch (Throwable t){
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    private void setUpCameraScreen(){
+        cameraView = (SurfaceView) viewer.findViewById(getResources().getIdentifier("surface_view", "id", appResourcesPackage));
+        textView = (TextView) viewer.findViewById(getResources().getIdentifier("txtView", "id", appResourcesPackage));
+        Button button = (Button) viewer.findViewById(getResources().getIdentifier("btnHide", "id", appResourcesPackage));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hide) {
+                    hide = false;
+                    textView.setVisibility(View.VISIBLE);
+                }
+                 else {
+                    hide = true;
+                    textView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getActivity().getWindow().getContext()).build();
+        if (!textRecognizer.isOperational()) {
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                final android.widget.Toast toast = android.widget.Toast.makeText(
+                  getActivity().getWindow().getContext(),
+                  "not operational",
+                  android.widget.Toast.LENGTH_LONG 
+                    );
+                    toast.show();
+                }
+            });
+        } else {
+            cameraSource = new CameraSource.Builder(getActivity().getWindow().getContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        public void run() {
+                        final android.widget.Toast toast = android.widget.Toast.makeText(
+                          getActivity().getWindow().getContext(),
+                          "Surface created",
+                          android.widget.Toast.LENGTH_LONG 
+                            );
+                            toast.show();
+                        }
+                    });
+                    try{
+                        cameraSource.start(cameraView.getHolder());
+                    }
+                    catch(IOException e){
+                        getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                            final android.widget.Toast toast = android.widget.Toast.makeText(
+                              getActivity().getWindow().getContext(),
+                              "Could not start Camera",
+                              android.widget.Toast.LENGTH_LONG 
+                                );
+                                toast.show();
+                            }
+                        });
+                    }
+                    
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0){
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0;i<items.size();++i){
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                    Pattern p = Pattern.compile("DIN\\s\\d{8}");
+                                    Matcher m = p.matcher(item.getValue().toString());
+                                    if (m.matches()){
+                                        Context context = getActivity().getWindow().getContext();
+                                        apiCall(item.getValue().toString());
+                                    }
+                                }
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
