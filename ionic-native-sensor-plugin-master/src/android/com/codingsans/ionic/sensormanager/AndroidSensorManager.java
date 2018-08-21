1951c6655ac53fc16ae83349557ce02d0ff123ee
Jenis Jesuratnam <Jenis_Jesuratnam@hotmail.com>
2018-08-20 23:23:02 -0400

    Its all working!

---
+++ ionic-native-sensor-plugin-master/src/android/com/codingsans/ionic/sensormanager/AndroidSensorManager.java
@@ -0,0 +1,200 @@
+package com.codingsans.ionic.sensormanager;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import android.Manifest;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import android.view.ViewGroup;
+import android.os.Handler;
+
+import android.content.pm.PackageManager;
+
+public class AndroidSensorManager extends CordovaPlugin {
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private CallbackContext callbackContext;
+    private JSONObject data = new JSONObject();
+    private CameraScreenFrag frag;
+    private int containerViewId = 20;
+    CallbackContext callbackContext2;
+    String dinChecker = "";
+
+    private static final String [] permissions = {
+        Manifest.permission.CAMERA
+      };
+
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        mSensorManager = (SensorManager) cordova.getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for(int r:grantResults){
+            if(r == PackageManager.PERMISSION_DENIED){
+                cordova.getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                    final android.widget.Toast toast = android.widget.Toast.makeText(
+                      cordova.getActivity().getWindow().getContext(),
+                      "Something went wrong getting permissions",
+                      android.widget.Toast.LENGTH_LONG 
+                        );
+                        toast.show();
+                    }
+                });
+              return;
+            }
+          }
+          if (requestCode == 0) {
+            startScanner(callbackContext);
+          }
+    }
+
+    @Override
+    public void onDestroy() {
+        mSensorManager.unregisterListener(listener);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("start".equals(action)) {
+            mSensorManager.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        } else if ("stop".equals(action)) {
+            if (cordova.hasPermission(permissions[0])) {
+                return startScanner(callbackContext);
+              } else {
+                this.callbackContext2 = callbackContext;
+                cordova.requestPermissions(this, 0, permissions);
+                return true;
+              }
+              //startScanner();
+        }
+        else if ("getDin".equals(action)){
+              dinChecker = frag.dinNumber; 
+              if (!dinChecker.equals("")){
+                data = new JSONObject();
+                try {
+                  data.put("din", dinChecker);
+                } catch(JSONException e) {}
+                
+                cordova.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                    ((ViewGroup)webView.getView()).bringToFront();
+                    }
+                });
+                try{
+                FragmentTransaction ft = cordova.getActivity().getFragmentManager().beginTransaction();
+                frag.dinNumber = "";
+                ft.remove(frag).commit();
+                frag = null;
+                }
+                catch(Exception e2){
+                    try {
+                        data.put("errormes", e2.toString());
+                      } catch(JSONException e) {}
+                }
+                callbackContext.success(this.data);
+                }
+              else{
+                data = new JSONObject();
+                try {
+                  data.put("din", dinChecker);
+                } catch(JSONException e) {}
+                callbackContext.error(this.data);
+              }
+              return true;
+        } 
+        else if ("close".equals(action)){
+            //frag.cameraSource.stop();
+        //     if (frag != null){
+        //      cordova.getActivity().runOnUiThread(new Runnable() {
+        //             @Override
+        //             public void run() {
+        //             ((ViewGroup)webView.getView()).bringToFront();
+        //             }
+        //     });
+        //     fragmentTransaction.remove(frag);
+        //     frag = null;
+        // }
+            return true;
+        }
+        else if ("getCurrent".equals(action)) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, this.data);
+            callbackContext.sendPluginResult(result);
+            return true;
+        }
+        return false;  // Returning false results in a "MethodNotFound" error.
+    }
+
+    private SensorEventListener listener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+          if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+              data = new JSONObject();
+              try {
+                  data.put("x", event.values[0]);
+                  data.put("y", event.values[1]);
+                  data.put("z", event.values[2]);
+              } catch(JSONException e) {}
+          }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // unused
+        }
+    };
+
+    private boolean startScanner(CallbackContext callbackContext){
+        if (cordova.getActivity()!= null && frag == null){
+        try{
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                
+        frag = new CameraScreenFrag();
+        frag.dinNumber = "";
+        FrameLayout containerView = (FrameLayout)cordova.getActivity().findViewById(containerViewId);
+        if(containerView == null){
+            containerView = new FrameLayout(cordova.getActivity().getApplicationContext());
+            containerView.setId(containerViewId);
+            FrameLayout.LayoutParams containerLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            cordova.getActivity().addContentView(containerView, containerLayoutParams);
+        }
+        containerView.bringToFront();
+        FragmentManager fragmentManager = cordova.getActivity().getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(containerView.getId(), frag);
+        fragmentTransaction.commit();
+                }
+            });
+    }
+    catch(Exception e){
+        try {
+            data.put("errorMessage", e.toString());
+        } catch(JSONException e1) {}
+        callbackContext.error(this.data);
+    }
+}
+        return true;
+    }
+}
---
+++ ionic-native-sensor-plugin-master/src/android/com/codingsans/ionic/sensormanager/CameraScreenFrag.java
@@ -0,0 +1,193 @@
+package com.codingsans.ionic.sensormanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+import android.content.Context;
+
+//Vision Stuff
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//Vision Stuff
+
+//Camera Stuff
+import android.graphics.Point;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.SurfaceHolder;
+import android.util.SparseArray;
+//Camera Stuff
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CameraScreenFrag extends Fragment{
+   
+    View viewer;
+    String appResourcesPackage;
+    SurfaceView cameraView;
+    TextView textView;
+    public String dinNumber = "";
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+    boolean hide = true;
+    boolean firstTime = true;
+    String url = "https://health-products.canada.ca/api/drug/drugproduct/?lang=en&type=json&din=";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        appResourcesPackage = getActivity().getPackageName();  
+        viewer = inflater.inflate(getResources().getIdentifier("camera_view_screen", "layout", appResourcesPackage), container, false);
+        setUpCameraScreen();
+        return viewer;
+    }
+
+    protected void apiCall(String id, Point[] points){
+
+        String tempUrl = url + id.substring(4);
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getWindow().getContext());
+        final Point[] pointsToSend = points;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response != null) {
+                            try{
+                                JSONArray objarr = new JSONArray(response.toString());
+                                JSONObject obj = objarr.getJSONObject(0);
+                                final String toOutputNum = obj.getString("drug_identification_number");
+                                dinNumber = toOutputNum;
+                                showText(toOutputNum, pointsToSend[3]);
+                                // returnIntent.putExtra("result", toOutput);
+                                // returnIntent.putExtra("result2", obj.getString("drug_identification_number"));
+                                // returnIntent.putExtra("result3", obj.getString("company_name"));
+                                // returnIntent.putExtra("result4", obj.getString("descriptor"));
+                            }
+                            catch (Throwable t){
+                            }
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    private void showText(String dinNum, Point p1){
+        final String toDisplay = dinNum;
+        final Point pointToUse = p1;
+        getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    textView.setText(toDisplay);
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setX(pointToUse.x);
+                    textView.setY(pointToUse.y);
+                }
+            });
+    }
+
+    private void setUpCameraScreen(){
+        cameraView = (SurfaceView) viewer.findViewById(getResources().getIdentifier("surface_view", "id", appResourcesPackage));
+        textView = (TextView) viewer.findViewById(getResources().getIdentifier("txtView", "id", appResourcesPackage)); 
+        textView.setVisibility(View.INVISIBLE);
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getActivity().getWindow().getContext()).build();
+        if (!textRecognizer.isOperational()) {
+            getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                final android.widget.Toast toast = android.widget.Toast.makeText(
+                  getActivity().getWindow().getContext(),
+                  "not operational",
+                  android.widget.Toast.LENGTH_LONG 
+                    );
+                    toast.show();
+                }
+            });
+        } else {
+            //if (firstTime){
+            cameraSource = new CameraSource.Builder(getActivity().getWindow().getContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                    try{
+                        cameraSource.start(cameraView.getHolder());
+                    }
+                    catch(IOException e){
+                        getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                            final android.widget.Toast toast = android.widget.Toast.makeText(
+                              getActivity().getWindow().getContext(),
+                              "Could not start Camera",
+                              android.widget.Toast.LENGTH_LONG 
+                                );
+                                toast.show();
+                            }
+                        });
+                    }
+                    
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+        // }
+        // else{
+            
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0){
+                        for(int i=0;i<items.size();++i){
+                            TextBlock item = items.valueAt(i);
+                            Pattern p = Pattern.compile("DIN.?\\d{8}");
+                            Matcher m = p.matcher(item.getValue().toString());
+                            if (m.matches()){
+                                apiCall(item.getValue().toString(), item.getCornerPoints());
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
